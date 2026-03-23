9d2e5e9ae0c1655edca0981c483845d55480deb2
sufyaancs9boop <sufyaancs9@gmail.com>
2026-03-23 01:54:50 -0700

    3rd commit

--- src/main.py
+++ src/main.py
@@ -11,3 +11,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/healthz")
+def read_api_health():
+    return {"message": "OK"}
