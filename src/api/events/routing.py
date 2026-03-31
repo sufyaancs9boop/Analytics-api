4ba4c50360420ce9d52fb6523a74758f55811623
Sufyan <sufyaancs9@gmail.com>
2026-03-31 19:53:55 +0500

    routes

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,13 +1,21 @@
 from fastapi import APIRouter
-from .schemas import EventSchema
+from .schemas import EventListSchema, EventSchema
 router = APIRouter()
 
 @router.get("/")
-def read_events():
-    return{
-        "results":[1,2,3,4,5]
-    }
+def read_events()-> EventListSchema:
+    return EventListSchema(
+        results=[EventSchema(id=1), EventSchema(id=2), EventSchema(id=3)],
+        count=3
+    )
     
+#get data here
 @router.get("/{event_id}")
 def read_event(event_id: int) -> EventSchema:
     return EventSchema(id=event_id)
+
+#post data here
+@router.post("/")
+def create_event(data: dict = {}) -> EventSchema:
+    print(data)
+    return EventSchema(id=123)
--- src/api/events/schemas.py
+++ src/api/events/schemas.py
@@ -1,4 +1,8 @@
 from pydantic import BaseModel, Field
+from typing import List
 
 class EventSchema(BaseModel):
     id: int = Field(..., description="Unique identifier for the event")
+    
+class EventListSchema(BaseModel):
+    results: list[EventSchema]
