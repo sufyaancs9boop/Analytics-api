9758c0798354de80657321644d471f618a00c7d5
sufyaancs9boop <sufyaancs9@gmail.com>
2026-04-19 05:24:13 -0700

    changed in the config.py for db_url truncation issues

--- src/api/db/config.py
+++ src/api/db/config.py
@@ -1,4 +1,14 @@
 from decouple import config as decouple_config
 
-DATABASE_URL: str = str(decouple_config("DATABASE_URL", default=""))
-DB_TIMEZONE :str = str(decouple_config("DB_TIMEZONE", default="UTC"))
+DB_HOST = decouple_config("DB_HOST", default="")
+DB_PORT = decouple_config("DB_PORT", default="5432")
+DB_USER = decouple_config("DB_USER", default="")
+DB_PASSWORD = decouple_config("DB_PASSWORD", default="")
+DB_NAME = decouple_config("DB_NAME", default="")
+
+DATABASE_URL: str = (
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
+)
+
+DB_TIMEZONE: str = str(decouple_config("DB_TIMEZONE", default="UTC"))
